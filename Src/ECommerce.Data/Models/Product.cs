402dae30bc49c3850f50af57623909268ed90602
Almir Alves de Freitas Junior <almirafjr.dev@gmail.com>
2022-09-29 15:31:33 -0300

    feat: add scaffolded models from database to be used by entity framework core

---
+++ Src/ECommerce.Data/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace ECommerce.Data.Models
+{
+    public partial class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = null!;
+        public short? Amount { get; set; }
+        public decimal Price { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+    }
+}
