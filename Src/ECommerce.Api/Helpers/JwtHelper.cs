103e56474e46d3d5a9b127175d7a3c05a39b9984
Almir Alves de Freitas Junior <almirafjr.dev@gmail.com>
2022-09-30 19:47:39 -0300

    feat: add jwt and regex helpers

---
+++ Src/ECommerce.Api/Helpers/JwtHelper.cs
@@ -0,0 +1,17 @@
+using System.Security.Claims;
+
+namespace ECommerce.Api.Utils;
+
+public static class JwtHelper
+{
+    public static Dictionary<string, string> ClaimsToDictionary(IEnumerable<Claim> claims)
+        => claims.ToDictionary(c => c.Type, c => c.Value);
+
+    public static Guid? GetSellerGuid(Dictionary<string, string> claimsDictionary)
+    {
+        claimsDictionary.TryGetValue("sellerId", out var sellerId);
+        Guid.TryParse(sellerId, out var sellerGuid);
+
+        return sellerGuid;
+    }
+}
