1d204248b3c5f0f61bb5f6bda257c3e3869b708e
Almir Alves de Freitas Junior <almirafjr.dev@gmail.com>
2022-09-30 19:44:21 -0300

    feat: add seller and login services

---
+++ Src/ECommerce.Api/Services/SellerService.cs
@@ -0,0 +1,48 @@
+using System.Net;
+using ECommerce.Contracts.Dtos.Jwt;
+using ECommerce.Contracts.Dtos.Seller;
+using ECommerce.Contracts.Interfaces.Repositories;
+using ECommerce.Contracts.Interfaces.Services;
+
+namespace ECommerce.Api.Services;
+
+public class SellerService : ISellerService
+{
+    private readonly ISellerRepository _sellerRepository;
+
+    public SellerService(ISellerRepository sellerRepository)
+    {
+        _sellerRepository = sellerRepository;
+    }
+
+    public async Task<(SellerDto?, HttpStatusCode)> CreateSeller(CreateSellerDto sellerDto)
+    {
+        var (sellerModel, statusCode) = await _sellerRepository.CreateSeller(sellerDto.ToModel());
+
+        if (sellerModel is null)
+            return (null, statusCode);
+
+        return (SellerDto.FromModel(sellerModel), statusCode);
+    }
+
+    public Task<HttpStatusCode> DeleteSeller(Guid sellerId)
+        => _sellerRepository.DeleteSeller(sellerId);
+
+    public async Task<(SellerDto?, HttpStatusCode)> GetSellerById(Guid sellerId)
+    {
+        var (sellerModel, statusCode) = await _sellerRepository.GetSellerById(sellerId);
+
+        if (sellerModel is null)
+            return (null, statusCode);
+
+        return (SellerDto.FromModel(sellerModel), statusCode);
+    }
+
+    public Task<HttpStatusCode> UpdateSeller(Guid sellerId, UpdateSellerDto seller)
+    {
+        var sellerModel = seller.ToModel();
+        sellerModel.Id = sellerId;
+
+        return _sellerRepository.UpdateSeller(sellerModel);
+    }
+}
