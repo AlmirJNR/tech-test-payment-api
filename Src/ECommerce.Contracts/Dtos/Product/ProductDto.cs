3cd8de7029bd5b1bedfa4fbfd1c7d6c582626603
Almir Alves de Freitas Junior <almirafjr.dev@gmail.com>
2022-10-02 15:57:27 -0300

    feat: add product dtos

---
+++ Src/ECommerce.Contracts/Dtos/Product/ProductDto.cs
@@ -0,0 +1,21 @@
+namespace ECommerce.Contracts.Dtos.Product;
+
+public readonly record struct ProductDto()
+{
+    public Guid Id { get; init; } = Guid.Empty;
+    public string Name { get; init; } = null!;
+    public short? Amount { get; init; } = null;
+    public decimal Price { get; init; } = 0;
+    public DateTime? CreatedAt { get; init; } = null;
+    public DateTime? UpdatedAt { get; init; } = null;
+
+    public static ProductDto FromModel(Data.Models.Product productModel) => new()
+    {
+        Id = productModel.Id,
+        Name = productModel.Name,
+        Amount = productModel.Amount,
+        Price = productModel.Price,
+        CreatedAt = productModel.CreatedAt,
+        UpdatedAt = productModel.UpdatedAt
+    };
+}
