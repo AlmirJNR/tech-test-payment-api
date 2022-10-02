ba17ca88e0d5cd0b16b5040a05323f8eed9e01cc
Almir Alves de Freitas Junior <almirafjr.dev@gmail.com>
2022-10-02 16:25:21 -0300

    fix: parameter name

--- Src/ECommerce.Contracts/Interfaces/Services/IProductService.cs
+++ Src/ECommerce.Contracts/Interfaces/Services/IProductService.cs
@@ -5,8 +5,8 @@ namespace ECommerce.Contracts.Interfaces.Services;
 
 public interface IProductService
 {
-    public Task<(ProductDto?, HttpStatusCode)> CreateProduct(CreateProductDto productModel);
+    public Task<(ProductDto?, HttpStatusCode)> CreateProduct(CreateProductDto productDto);
     public Task<HttpStatusCode> DeleteProduct(Guid productId);
     public Task<(ProductDto?, HttpStatusCode)> GetProductById(Guid productId);
-    public Task<HttpStatusCode> UpdateProduct(Guid productId, UpdateProductDto productModel);
+    public Task<HttpStatusCode> UpdateProduct(Guid productId, UpdateProductDto productDto);
 }
