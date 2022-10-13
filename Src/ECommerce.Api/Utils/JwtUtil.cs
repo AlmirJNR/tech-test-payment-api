1250cb4214a0ef80c898243bea52f49fe86d2942
Almir Alves de Freitas Junior <almirafjr.dev@gmail.com>
2022-10-12 22:38:53 -0300

    refactor: change method return type to defaulted empty guid

--- Src/ECommerce.Api/Utils/JwtUtil.cs
+++ Src/ECommerce.Api/Utils/JwtUtil.cs
@@ -7,13 +7,11 @@ public static class JwtUtil
     public static Dictionary<string, string> ClaimsToDictionary(IEnumerable<Claim> claims)
         => claims.ToDictionary(c => c.Type, c => c.Value);
 
-    public static Guid? GetSellerGuid(Dictionary<string, string> claimsDictionary)
+    public static Guid GetSellerGuid(Dictionary<string, string> claimsDictionary)
     {
         claimsDictionary.TryGetValue("sellerId", out var sellerId);
         Guid.TryParse(sellerId, out var sellerGuid);
 
-        return sellerGuid == Guid.Empty
-            ? null
-            : sellerGuid;
+        return sellerGuid;
     }
 }
